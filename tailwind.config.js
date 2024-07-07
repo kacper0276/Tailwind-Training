caeffffb34442c70dda8c57f454de3aa244a88c1
kacper0276 <kacper0276@op.pl>
2024-07-08 00:17:20 +0200

    Custom class tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        phone: "375px",
+      },
+    },
   },
   plugins: [],
 };
